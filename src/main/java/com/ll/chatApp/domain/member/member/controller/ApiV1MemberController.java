da8ac528ad8efd8cf2b0d7160d8c2004ec5af901
junghyun13 <ljh041314@gmail.com>
2025-01-10 15:42:53 +0900

    JWT accessToken발급 쿠키에 담아보내기

--- src/main/java/com/ll/chatApp/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/ll/chatApp/domain/member/member/controller/ApiV1MemberController.java
@@ -5,8 +5,11 @@ import com.ll.chatApp.domain.member.member.entity.Member;
 import com.ll.chatApp.domain.member.member.service.MemberService;
 import com.ll.chatApp.domain.member.member.dto.MemberRequest;
 import com.ll.chatApp.domain.member.member.service.MemberService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import com.ll.chatApp.global.jwt.JwtProvider;
 import com.ll.chatApp.global.rsData.RsData;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
@@ -22,10 +25,12 @@ public class ApiV1MemberController {
         return new RsData<>("200", "회원가입에 성공하였습니다.", new MemberDto(member));
     }
     @PostMapping("/login")
-    public RsData<String> login(@Valid @RequestBody MemberRequest memberRequest) {
+    public RsData<Void> login(@Valid @RequestBody MemberRequest memberRequest, HttpServletResponse response) {
         Member member = memberService.getMember(memberRequest.getUsername());
-        String token = jwtProvider.genAccessToken(member);
-        return new RsData<>("200", "로그인에 성공하였습니다.", token);
+        String token = jwtProvider.genAccessToken(member); //토큰 생성
+        // 응답 데이터에 accessToken 이름으로 토큰을 발급
+        response.addCookie(new Cookie("accessToken", token));
+        return new RsData<>("200", "로그인에 성공하였습니다.");
     }
     @GetMapping("/logout")
     public void logout(){
